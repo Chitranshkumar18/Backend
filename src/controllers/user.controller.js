ec4854554de6546aa59f8d55afea5e69b7737120
ChitranshKumar18 <chitranshkumar730@gmail.com>
2026-09-24 23:47:07 +0530

    feat: add subscription model and controllers for changing password and fetching current user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -246,6 +246,41 @@ const refreshAccessToken = asyncHandler(async(req,res) =>{
 })
 
 
+
+const ChangeCurrentPassword = asyncHandler(async(req, res)=>{
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.ispasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword
+    await user.save({ValiditionBeforeSave: false})
+
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password changed successfully"))
+})
+
+
+
+const getCurrentUser = asyncHandler(async(req, res) =>{
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "Current user fetched successfully"
+    ))
+})
+
+
+
     
     
 
@@ -256,5 +291,8 @@ export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    ChangeCurrentPassword,
+
+
 }
